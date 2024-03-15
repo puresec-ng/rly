231fff1bd265e0994af272ea0af4b2a015d1f45f
impozzible <30698707+impozzible@users.noreply.github.com>
2024-03-16 00:42:30 +0100

    fix

--- src/programs/token-swap/initSetupTransaction.ts
+++ src/programs/token-swap/initSetupTransaction.ts
@@ -39,7 +39,7 @@ interface initializeLinearPriceCurveTxParams {
   walletPubKey: web3.PublicKey;
   connection: any;
   initialTokenBLiquidity: BN;
-    poolTokenMintKeypair: Keypair;
+    poolTokenMintKeypair: web3.Keypair;
 }
 
 interface initializeLinearPriceCurveParams {
@@ -56,7 +56,7 @@ interface initializeLinearPriceCurveParams {
   wallet: Wallet;
   connection: any;
   initialTokenBLiquidity: BN;
-    poolTokenMintKeypair: Keypair;
+    poolTokenMintKeypair: web3.Keypair;
 }
 
 interface initializeLinearPriceCurveOpts {
